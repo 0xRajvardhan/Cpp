3baa8affa940a9b79e0f9866eb76d0e5489a2c11
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-05-11 12:00:48 +0530

    conditionals

---
+++ if-else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cin >> a >> b;
+
+    if (a > b)
+    {
+        cout << "a is greater than b" << endl;
+    }else
+    {
+        cout << "b is greater than a" << endl;
+    }
+}
