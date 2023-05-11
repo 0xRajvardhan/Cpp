26bdecb31a7d787e3b395c15304232896c1f57aa
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-05-11 12:08:51 +0530

    -

---
+++ if-else/PositiveOrNegative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int a;
+    cin >> a;
+
+    if (a > 0)
+    {
+        cout << "a is positive" << endl;
+    }else
+    {
+        cout << "a is negative" << endl;
+    }
+}
