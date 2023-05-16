5e91340137661e9d1dcda9033ca2f74b6d504c2c
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-05-16 20:09:47 +0530

    bitwise operators

---
+++ bitwise_operators/ForLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter the value of n: " << endl;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i<<endl;
+    }
+}
---
+++ bitwise_operators/Prime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "enter the value of n: ";
+    cin >> n;
+    for (int i = 2; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "not prime" << endl;
+            break;
+        }
+        else
+        {
+            cout << "prime" << endl;
+        }
+    }
+}
