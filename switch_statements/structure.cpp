10c4afc61c17c8cab2748c288b761c95cff451c2
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-06-15 01:36:29 +0530

    code structure of switch cases

---
+++ switch_statements/structure.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 2;
+
+    cout << endl;
+
+    switch (num)
+    {
+
+    case 1:
+        cout << "First" << endl;
+        break;
+    case 2:
+        cout << "Second" << endl;
+        break;
+
+    default:
+        cout << "This is the default case" << endl;
+    }
+}
