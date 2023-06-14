87a0a8dca218e94d1a96e7695dcd8ebd9028d158
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-06-15 01:44:36 +0530

    built a simple calculator using the concept of switch-cases

---
+++ switch_statements/switch-case_Calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    char op;
+    cout << "Enter the operator you want to perform: ";
+    cin >> op;
+
+    switch (op)
+    {
+    case '+':
+        cout << (a + b) << endl;
+        break;
+    case '-':
+        cout << (a - b) << endl;
+        break;
+    case '*':
+        cout << (a * b) << endl;
+        break;
+    case '/':
+        cout << (a / b) << endl;
+        break;
+    default:
+        cout << "Please enter a valid operator" << endl;
+    }
+}
