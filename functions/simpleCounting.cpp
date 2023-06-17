3b5ee9346dd81d9cbaa29a875d43df9d06fac47b
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-06-17 14:52:42 +0530

    simple counting function

---
+++ functions/simpleCounting.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void printCounting(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    printCounting(n);
+
+    return 0;
+}
